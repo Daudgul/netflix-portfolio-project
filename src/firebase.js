35c6585d6bc1a712906fd12c458edaf2d7e2e370
daud gul <codrdaud@gmail.com>
2023-07-24 12:58:41 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAonZhYf3-r7TtjY_SYIfHjVESOpARjCmY",
+    authDomain: "netflix-clone-portfolio-aebe3.firebaseapp.com",
+    projectId: "netflix-clone-portfolio-aebe3",
+    storageBucket: "netflix-clone-portfolio-aebe3.appspot.com",
+    messagingSenderId: "60999195997",
+    appId: "1:60999195997:web:aeb256be6ae5971f855cb4"
+  };
