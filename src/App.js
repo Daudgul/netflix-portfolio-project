e1eba3497da09671571c9cb0048fc0687d72888e
daud gul <codrdaud@gmail.com>
2023-07-26 14:36:52 +0530

    added login screen page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen/>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={user ? <HomeScreen/> : <LoginScreen/>} />
+            <Route path="/about" element={ <h2>Hey what up Daud</h2>} />
+          </Routes>          
+      </BrowserRouter>
+     
     </div>
   );
 }
